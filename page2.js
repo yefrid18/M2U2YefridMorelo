a62e0f6e94b20359c3552f058aee0b2fa543c6fa
yefrid18 <yefrid18@gmail.com>
2022-06-24 20:56:59 -0500

    ejercicio 2 listo html y js

--- page2.js
+++ page2.js
@@ -1,19 +1,28 @@
-//Llenado de variables con datos del html
-let a = document.getElementById('numero1')
-let b = document.getElementById('numero2')
-let c = document.getElementById('numero3')
-let d = document.getElementById('numero4')
+
+let a = document.getElementById("numero1")
+let b = document.getElementById("numero2")
+let c = document.getElementById("numero3")
+let d = document.getElementById("numero4")
 let botoncito = document.getElementById('boton')
 let numMayor = document.getElementById('numeroMayor')
 let numMenor = document.getElementById('numeroMenor')
 
 
-var lista_numeros =[a,b,c,d]
+let lista_numeros = []; 
 
 
 botoncito.addEventListener("click", () => {
 
+    if ((a.value == b.value) || (a.value == c.value) || (a.value == d.value) || (b.value == c.value) || (b.value == d.value) || (c.value == d.value)){
+        alert("No se pueden digitar numeros repetidos, favor revisar")    
+    
+    }else{  
+
+    lista_numeros=[parseInt(a.value),parseInt(b.value),parseInt(c.value),parseInt(d.value)]    
     lista_numeros.sort(function(a, b){return a - b});    
     numMayor.innerHTML = 'El numero mayor es:' + lista_numeros[3]
     numMenor.innerHTML = 'El numero menor es:' + lista_numeros[0]
+
+    }
+       
 })
