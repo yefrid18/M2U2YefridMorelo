389be94d8d4af97d11b7284619b0b8e17e20cebd
yefrid18 <yefrid18@gmail.com>
2022-06-26 16:22:31 -0500

    Agregar Html Ejercicio 1

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let estudiante = document.getElementById("estudiante")
+let cantMaterias = document.getElementById("cantMaterias")
+let botoncito = document.getElementById("boton")
+let ValorPagar = document.getElementById("Valor_Pagar")
+
+botoncito.addEventListener("click", () => {
+
+    let valorMaterias = 0
+    const papeleria = 20000
+    const carnet = 8000
+    const pagoxdescuento = 0.80
+
+    for(let i=1;i<=cantMaterias.value;i++) {
+        let valorMateria = prompt("Digite valor materia "+(i))
+        valorMateria = parseInt(valorMateria)
+        valorMaterias += valorMateria        
+    } 
+    
+    ValorPagar.innerHTML = "El valor total a pagar es " + (valorMaterias * pagoxdescuento + papeleria + carnet)
+
+})
